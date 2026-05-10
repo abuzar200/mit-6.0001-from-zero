62860b965436ff60004f25613467eb1f75e7f89b
Abuzar <kenny123sabuzar@gmail.com>
2026-05-10 20:44:45 +0530

    day_11: Demonstration of Function, scope and Global Keyword

---
+++ day_11.py
@@ -0,0 +1,30 @@
+def calculate_discount(price, percent):
+    """returns calculated discount"""
+    return price * (1 - percent / 100)
+
+
+def apply_gst(price, rate=18):
+    """returns the gst applied amount"""
+    return price * (1 - rate / 100)
+
+
+def final_price(base, discount):
+    after_discount = calculate_discount(base, discount)
+    return apply_gst(after_discount)
+
+
+total = 1000
+
+
+def broken_add(amount):
+    total = total + amount
+    return total
+
+
+def correct_add(amount):
+    global total
+    total = total + amount
+    return total
+
+
+print(final_price(2000, 10))
